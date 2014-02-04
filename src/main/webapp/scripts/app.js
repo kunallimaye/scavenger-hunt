0bdc256f0136d05a8ce13ab2d50ca972dec72133
Kunal Limaye <kunallimaye@gmail.com>
2014-02-04 11:59:56 +1100

    Added openshift profile to pom.xml. Added angulasjs scaffolding

---
+++ src/main/webapp/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('scavengerhunt',['ngRoute','ngResource'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/',{templateUrl:'views/landing.html',controller:'LandingPageController'})
+      .otherwise({
+        redirectTo: '/'
+      });
+  }])
+  .controller('LandingPageController', function LandingPageController() {
+  })
+  .controller('NavController', function NavController($scope, $location) {
+    $scope.matchesRoute = function(route) {
+        var path = $location.path();
+        return (path === ("/" + route) || path.indexOf("/" + route + "/") == 0);
+    };
+  });
