094171b3ada87866dbeadb0510d8e1a5450f97ca
Kunal Limaye <kunallimaye@gmail.com>
2014-02-04 15:23:38 +1100

    Added a util class for persistence unit name

---
+++ src/main/java/com/kunal/demo/scavengerhunt/utils/Persistence.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.kunal.demo.scavengerhunt.utils;
+
+import java.io.Serializable;
+
+/**
+ * @author kunallimaye
+ *
+ */
+public class Persistence implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1269465069060572752L;
+
+	public static final String PERSISTENCE_UNIT = "scavenger-hunt";
+}
